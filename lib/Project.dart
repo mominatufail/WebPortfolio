e73afe7da2f7352825bca222289a4597a7ca5603
Momina Tufail <momnatufail30@gmail.com>
2025-01-06 21:30:44 +0500

    files created for the project

---
+++ lib/Project.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ProjectCard extends StatelessWidget {
+  final String title;
+  final String description;
+  final String url;
+
+  ProjectCard({required this.title, required this.description, required this.url});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            blurRadius: 5,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      padding: EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.purple.shade700,
+            ),
+          ),
+          SizedBox(height: 5),
+          Text(
+            description,
+            style: TextStyle(
+              fontSize: 14,
+              color: Colors.grey.shade700,
+            ),
+          ),
+          SizedBox(height: 10),
+          TextButton(
+            onPressed: () {}, // Add URL launcher logic here
+            child: Text('View on GitHub'),
+            style: TextButton.styleFrom(
+              backgroundColor: Colors.purple,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
