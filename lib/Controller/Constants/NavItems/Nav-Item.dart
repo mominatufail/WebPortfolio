4e5386776b210fb06ad679182eff7afdc8f234be
Momina Tufail <momnatufail30@gmail.com>
2025-01-11 21:51:20 +0500

    skill item created

---
+++ lib/Controller/Constants/NavItems/Nav-Item.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+List<String>NavTitles=[
+  'Home',
+  'Skills',
+  'Projects',
+  'Contacts',
+];
+
+List<IconData> NavIcons = [
+  Icons.home,
+  Icons.handyman_outlined,
+  Icons.apps,
+  Icons.contact_page_outlined,
+];
