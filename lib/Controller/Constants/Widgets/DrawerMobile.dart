c540289b151bd0ce61f86eea914b3930623b03ec
Momina Tufail <momnatufail30@gmail.com>
2025-01-11 22:03:33 +0500

    code updated for drawer

---
+++ lib/Controller/Constants/Widgets/DrawerMobile.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/Constants/Colors/Colors.dart';
+import 'package:portfolio/Constants/Nav-Items/Nav-Items.dart';
+import 'package:webportfolio/Controller/Constants/Colors/Colors.dart';
+import 'package:webportfolio/Controller/Constants/NavItems/Nav-Item.dart';
+
+class DrawerMobile extends StatelessWidget {
+  const DrawerMobile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor:CustomColor.ScaffoldBg,
+      child: ListView(
+        children: [
+          Align(alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  left: 20,
+                  top: 20,
+                  bottom: 20),
+              child: IconButton(onPressed: (){Navigator.of(context).pop();}, icon: Icon(Icons.close)),
+            ),
+          ),
+          for(int i=0; i<NavIcons.length; i++)
+            ListTile(
+              contentPadding: EdgeInsets.symmetric(horizontal: 30.0),
+              titleTextStyle: TextStyle(color: CustomColor.whitePrimary,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 16),
+              onTap: (){},
+              leading: Icon(NavIcons[i]),
+              title: Text(NavTitles[i]),
+            )
+        ],
+      ),
+    );
+  }
+}
