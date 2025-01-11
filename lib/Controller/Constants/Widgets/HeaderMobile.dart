8c7f26e81bce1861d6398e15235b857f8a3a2be0
Momina Tufail <momnatufail30@gmail.com>
2025-01-11 22:21:36 +0500

    updated code

---
+++ lib/Controller/Constants/Widgets/HeaderMobile.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:webportfolio/Controller/Constants/Styles/styles.dart';
+
+
+class HeaderMobile extends StatelessWidget {
+  const HeaderMobile({super.key, this.onLogoTap, this.onMenuTap});
+  final VoidCallback? onLogoTap;
+  final VoidCallback? onMenuTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      height:50 ,
+      margin: EdgeInsets.fromLTRB(40, 5, 20, 5),decoration: kHeaderDecoration,
+      child: Row(
+        children: [
+          SiteLogo(onTap: onLogoTap),
+          Spacer(),IconButton(onPressed: onMenuTap, icon:Icon(Icons.menu)),SizedBox(width: 15,)
+        ],
+      ),
+    );
+  }
+}
