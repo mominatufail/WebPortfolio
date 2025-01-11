4a471648f2189171aa62e7a121e0e52ad45006a3
Momina Tufail <momnatufail30@gmail.com>
2025-01-11 22:13:30 +0500

    code added

---
+++ lib/Controller/Constants/Styles/styles.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:webportfolio/Controller/Constants/Colors/Colors.dart';
+
+BoxDecoration kHeaderDecoration = BoxDecoration(gradient:
+LinearGradient(colors: [Colors.transparent,CustomColor.bgLight1,]),
+    borderRadius: BorderRadius.circular(100));
