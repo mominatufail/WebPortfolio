02ccb5557ec8b9324a0249ef6098987c15255b34
Momina Tufail <momnatufail30@gmail.com>
2025-01-11 21:52:49 +0500

    updated skill item

---
+++ lib/Controller/Constants/Skill-Item/Skill-Item.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+const List<Map> platformItems = [
+  {
+    "img": 'assets/img_3.png',
+    'title': 'Android Dev'
+  },
+  {
+    "img": 'img_4.png',
+    'title': 'Web Dev'
+  }
+];
+
+const List<Map> skillItems = [
+  {
+    "img": 'assets/img_2.png',
+    'title': 'Flutter'
+  },
+  {
+    "img": 'assets/img_1.png',
+    'title': 'Dart'
+  }
+];
