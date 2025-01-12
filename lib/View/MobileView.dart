bcdf57936deeaa904c8031bf85eff4cffc2bec6c
Momina Tufail <momnatufail30@gmail.com>
2025-01-12 12:00:46 +0500

    code for mobile view updated

---
+++ lib/View/MobileView.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:webportfolio/Controller/Constants/Colors/Colors.dart';
+
+
+class MainMobile extends StatelessWidget {
+  const MainMobile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    final screenWidth = screenSize.width;
+    final screenHeight = screenSize.height;
+    return  Container(
+      margin: EdgeInsets.symmetric(
+          horizontal: 40,
+          vertical: 30
+      ),
+      height: screenHeight,
+      constraints: BoxConstraints(minHeight: 550),
+      child:Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          //avatar img
+          ShaderMask(
+              shaderCallback:(bounds){
+                return LinearGradient(colors: [
+                  CustomColor.ScaffoldBg.withOpacity(0.6),
+                  CustomColor.ScaffoldBg.withOpacity(0.6)]).
+                createShader(bounds);},
+              blendMode:BlendMode.srcATop,
+              child: Image.asset('assets/img.png',width: screenWidth)),
+          SizedBox(
+            height: 30,
+          ),
+          Text('Hi,\n I am Mahnoor Bukhari \n Flutter Developer',style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: CustomColor.whitePrimary,
+          ),),
+          SizedBox(height: 15,),
+          SizedBox(
+              width: 160,
+              child: ElevatedButton(onPressed: (){}, child: Text('Get in Touch',),))
+        ],
+      ) ,);
+  }
+}
