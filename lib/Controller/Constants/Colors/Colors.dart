6153ac34a83270d55c9a3af14d8f751ae9f1b4bd
Momina Tufail <momnatufail30@gmail.com>
2025-01-12 17:24:57 +0500

    color updated

--- lib/Controller/Constants/Colors/Colors.dart
+++ lib/Controller/Constants/Colors/Colors.dart
@@ -5,7 +5,7 @@ class CustomColor {
   static const Color bgLight2 = Color(0xff424657);
   static const Color textfieldbg = Color(0xffC8C9CE);
   static const Color hintDark = Color(0xff838cbd);
-  static const Color yellowSecondary = Color(0xffffff0d);
+  static const Color yellowSecondary = Color(0xffffff0d);//(0xFFEDE7F6);//
   static const Color yellowPrimary = Color(0xffFFAF29);
   static const Color whitePrimary = Color(0xfff2faf7);
   static const Color whiteSecondary = Color(0xffC8C9CE);
